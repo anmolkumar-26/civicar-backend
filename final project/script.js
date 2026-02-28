366dbfd012d583e2b2de044e389497a647ff2405
Anmol Kumar <pruchik2603@gmail.com>
2026-02-28 15:36:29 +0530

    Frontend deployed

--- final project/script.js
+++ final project/script.js
@@ -51,7 +51,7 @@ document.getElementById("reportForm").addEventListener("submit", async function(
   const type = document.getElementById("type").value;
 
   try {
-    const response = await fetch("http://localhost:5000/report", {
+    const response = await fetch("https://civicar-backend.onrender.com/report", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
